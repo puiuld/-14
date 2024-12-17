395b9c5963c05ab1bf78f5503644d04c423539ec
puiuld <puiuld47@gmail.com>
2024-12-17 09:07:12 +0200

    Add files via upload

---
+++ pd/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pd
+{/// <summary>
+/// Вывести на экран в столбик первые 10 натуральных чисел (циклы с условием, цикл с параметром)
+/// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadLine();
+        }
+        //Task1
+        //public static void Func()
+        //{
+        //    int x = 1;
+        //    while (x <= 10)
+        //    {
+        //        Console.WriteLine(x);
+        //        x++;
+        //    }
+        //}
+
+        //Task2
+        //public static void Func()
+        //{
+        //    int x = 1;
+        //    do
+        //    {
+        //        Console.WriteLine(x);
+        //        x++;
+        //    }
+        //    while (x <= 10);
+        //}
+
+        //Task3
+        //public static void Func()
+        //{
+        //    for(int x = 1 ;x<=10;x++)
+        //    {
+        //        Console.WriteLine(x);
+        //    }
+        //}
+    }
+}
