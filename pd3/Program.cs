3502b4553d1fc7b936e9ca0d98e70f2277e2aeb7
puiuld <puiuld47@gmail.com>
2024-12-17 09:38:18 +0200

    Add files via upload

---
+++ pd3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pd3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadLine();
+        }
+        public static void Func()
+        {
+            //for (int x = 1; x <= 5; x++)
+            //{
+            //    Console.WriteLine("Привет");
+            //}
+
+            //int x = 1;
+            //while (x <= 5)
+            //{
+            //    Console.WriteLine("Привет");
+            //    x++;
+            //}
+
+        }
+    }
+}
