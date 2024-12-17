7313d409cfc5f641c6be806a38a53d16defffc86
puiuld <puiuld47@gmail.com>
2024-12-17 09:31:09 +0200

    Add files via upload

---
+++ pd2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pd2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Vvod("n=");
+            Func();
+            Console.ReadLine();
+            
+        }
+        public static string Vvod(string message)
+        {
+            Console.Write(message);
+            return (Console.ReadLine());
+        }
+        public static void Func()
+        {
+            
+            for (int x = 1; x<=n; x++)
+            {
+                
+                Console.WriteLine(x);
+            }
+        }
+
+
+    }
+}
